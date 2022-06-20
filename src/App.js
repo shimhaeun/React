64d2d5924f40b633aa3a0af4e3e307813821a488
haeun <haeun62433@gmail.com>
2022-06-20 17:39:42 +0900

    create component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MyComponent from "./MyComponent";
+
+const App = () => {
+  return (
+    <MyComponent name="React" favoriteNumber={2}>
+      리액트
+    </MyComponent>
+  );
+};
+
+export default App;
