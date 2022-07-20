e61138e0131e5e12a81cb64c5ab30c2f4c1f4dd8
shimhaeun <haeun62433@gmail.com>
2022-07-21 01:38:17 +0900

    ref2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Component } from "react";
 import Counter from "./Counter";
 import EventPractice from "./EventPractice";
 import Info from "./info";
 import Say from "./Say";
+import ScrollBox from "./ScrollBox";
 import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  return <ValidationSample />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+        <button onClick={() => this.ScrollBox.ScrollToBottom()}>
+          맨 밑으로~
+        </button>
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -19,7 +19,8 @@ class ValidationSample extends Component {
         this.setState({
             clicked: true,
             validated: this.state.password === '0000'
-        })
+        });
+        this.input.focus();
     }
 
 
@@ -27,6 +28,7 @@ render() {
     return (
         <div>
             <input
+            ref={(ref) => this.input=ref}
             type="password"
             value={this.state.password}
             onChange={this.handleChange}
