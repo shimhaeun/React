941cbf61a78b3ed63ed3b0aa54b7adef86182e1a
shimhaeun <haeun62433@gmail.com>
2022-07-21 23:57:59 +0900

    key 설정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import Counter from "./Counter";
 import EventPractice from "./EventPractice";
 import Info from "./info";
+import IterationSample from "./IterationSample";
 import Say from "./Say";
 import ScrollBox from "./ScrollBox";
 import ValidationSample from "./ValidationSample";
@@ -9,12 +10,13 @@ import ValidationSample from "./ValidationSample";
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
-        <button onClick={() => this.ScrollBox.ScrollToBottom()}>
-          맨 밑으로~
-        </button>
-      </div>
+      <IterationSample />
+      // <div>
+      //   <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+      //   <button onClick={() => this.ScrollBox.ScrollToBottom()}>
+      //     맨 밑으로~
+      //   </button>
+      // </div>
     )
   }
 }
