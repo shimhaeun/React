b379235ae20ecfa1927438d1802883fec715afdd
shimhaeun <haeun62433@gmail.com>
2022-07-22 00:36:42 +0900

    데이터 추가 가능 구현

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const IterationSample = () => {
-   const names = ['눈사람', '얼음', '눈', '바람'];
-   const nameList = names.map((name,index) => <li key={index}>{name}</li>);
-   return <ul>{nameList}</ul>;
+   const [names, setNames] = useState ([
+    { id: 1, text: '눈사람' },
+    { id: 2,  text: '얼음' },
+    { id: 3,  text: '눈' },
+    { id: 4,  text: '바람' },
+   ]);
+   const [inputText, setInputText] = useState('');
+   const [nextId, setNextId] = useState(5); // 새로운 항목 추가할 때 사용할 id
+
+   const onChange = e => setInputText(e.target.value);
+   const onClick = () => {
+      const nextNames = names.concat({
+         id: nextId, // nextId값을 id로 설정하고
+         text: inputText
+      });
+      setNextId(nextId + 1); // nextId 값에 1을 더해 준다.
+      setNames(nextNames); // names 값을 업데이트한다.
+      setInputText(''); // inputText를 비운다.
+   };
+    
+   const nameList = names.map((name => <li key={name.id}>{name.text}</li>)); // key 설정
+   return (
+      <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+      </>
+   );
 };
+   
 
 export default IterationSample;
